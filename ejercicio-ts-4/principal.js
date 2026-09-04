eb02bed3f0562f6aa94c06b94ee8b2c828b8d01d
pilarvillegass <pilarvillegas7770@gmail.com>
2026-09-04 07:12:39 -0300

    ej4 ts

---
+++ ejercicio-ts-4/principal.js
@@ -0,0 +1,3 @@
+import { sumar } from "./sumar.js";
+const resultado = sumar(15, 27);
+console.log(`el resultado es: ${resultado}`);
---
+++ ejercicio-ts-4/principal.ts
@@ -0,0 +1,4 @@
+import { sumar } from "./sumar.js";
+
+const resultado = sumar(15, 27);
+console.log(`el resultado es: ${resultado}`);
