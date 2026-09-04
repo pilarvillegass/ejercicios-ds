47e429278dc44831f54dfc268a756ac51cea4305
pilarvillegass <pilarvillegas7770@gmail.com>
2026-09-04 07:01:43 -0300

    ej2 ts

---
+++ ejercicio-ts-2/multiplicar.js
@@ -0,0 +1,6 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+const resultado = multiplicar(6, 7);
+console.log(`el resultado es: ${resultado}`);
---
+++ ejercicio-ts-2/multiplicar.ts
@@ -0,0 +1,6 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+const resultado = multiplicar(6, 7);
+console.log(`el resultado es: ${resultado}`);
